de7c195deb842c099ddf1f19c758bdd239897cfb
Nasir Ahmed Momin <nasir.momin@mutualmobile.com>
2022-04-05 13:26:04 +0530

    Initial Commit

---
+++ Capture Memory Leak with XCTest/Capture_Memory_Leak_with_XCTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capture_Memory_Leak_with_XCTestApp.swift
+//  Capture Memory Leak with XCTest
+//
+//  Created by Nasir Ahmed Momin on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Capture_Memory_Leak_with_XCTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Capture Memory Leak with XCTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Capture Memory Leak with XCTest
+//
+//  Created by Nasir Ahmed Momin on 05/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
